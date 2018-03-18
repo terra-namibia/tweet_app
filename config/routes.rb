c63fc7391f0c8a8bd0421970e9c79005173ef711
Hirohito Terauchi <tera9319@yahoo.co.jp>
2018-03-19 00:42:29 +0900

    user,post,like追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,27 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/new'
-  get 'posts/:id' => 'posts#show'
-  post 'posts/create'
-  get 'posts/:id/edit' => 'posts#edit'  
+  post "likes/:post_id/create" => "likes#create"
+  post "likes/:post_id/destroy" => "likes#destroy"
 
-  get '/' => 'home#top'
+  post "users/:id/update" => "users#update"
+  get "users/:id/edit" => "users#edit"
+  post "users/create" => "users#create"
+  get "signup" => "users#new"
+  get "users/index" => "users#index"
+  get "users/:id" => "users#show"
+  post "login" => "users#login"
+  post "logout" => "users#logout"
+  get "login" => "users#login_form"
+  get "users/:id/likes" => "users#likes"
+
+  get "posts/index" => "posts#index"
+  get "posts/new" => "posts#new"
+  get "posts/:id" => "posts#show"
+  post "posts/create" => "posts#create"
+  get "posts/:id/edit" => "posts#edit"
+  post "posts/:id/update" => "posts#update"
+  post "posts/:id/destroy" => "posts#destroy"
+
+  get "/" => "home#top"
   get "about" => "home#about"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
