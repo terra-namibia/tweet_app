f143de6da22aa92017e4dc65e7cab3004caa6577
Hirohito Terauchi <tera9319@yahoo.co.jp>
2019-07-05 16:35:57 +0900

    fix spec

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -2,5 +2,10 @@ FactoryBot.define do
   factory :post do
     content {"MyString"}
     user_id {1}
+
+    trait :long_content do
+      content {"123456789012345678901234567890"}
+    end
+
   end
 end
